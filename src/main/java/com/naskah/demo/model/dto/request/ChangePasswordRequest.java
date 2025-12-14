972ab8863f43f82b17b6ebaee32f899e2d6518cb
MasasilaM <info@masa-silam.com>
2025-12-14 18:12:11 +0700

    Initial commit

--- src/main/java/com/naskah/demo/model/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/naskah/demo/model/dto/request/ChangePasswordRequest.java
@@ -1,7 +1,6 @@
 package com.naskah.demo.model.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -12,7 +11,5 @@ public class ChangePasswordRequest {
 
     @NotBlank
     @Size(min = 8, message = "Password must be at least 8 characters")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
-            message = "Password must contain at least one lowercase, one uppercase, one digit and one special character")
     private String newPassword;
 }
